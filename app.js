baea07a531826de503ca7d0742c40cf1f5c96c86
wkwiatek <wojtek.kwiatek@gmail.com>
2015-03-15 17:07:17 +0100

    Getting hardcoded list of songs

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/songs', function (req, res) {
+  var songs = [
+    { author: 'Foo Fighters', song: 'Pretender' },
+    { author: 'Alter Bridge', song: 'Blackbird' }
+  ];
+  res.json(songs);
+});
+
 var server = app.listen(3000, function() {
 
   var host = server.address().address;

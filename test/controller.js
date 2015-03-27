fc83cb792fde0850fa9b6dcda30451a8a5520319
wkwiatek <wojtek.kwiatek@gmail.com>
2015-03-27 20:25:05 +0100

    Final tests

--- test/controller.js
+++ test/controller.js
@@ -1,8 +1,10 @@
 var chai = require('chai');
 var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
+var rewire = require('rewire');
+var q = require('q');
 var expect = chai.expect;
-var controller = require('../app/controller');
+var controller = rewire('../app/controller');
 
 chai.use(sinonChai);
 
@@ -25,6 +27,39 @@ describe('#chat', function() {
   it('renders view with user', function() {
     controller.chat(this.req, this.res);
     expect(this.res.render).to.have.been.calledWith('chat', this.req);
+  });
+
+});
+
+describe('#songs', function() {
+
+  beforeEach(function() {
+    this.q = q.defer();
+    this.getSongs = sinon.stub().returns(this.q.promise);
+    this.res = {
+      json: sinon.spy()
+    };
+    controller.__set__('getSongs', this.getSongs);
+    controller.songs({
+      params: {
+        q: 'query'
+      }
+    }, this.res);
+  });
+
+  it('should call getSongs', function() {
+    expect(this.getSongs).to.have.been.called;
+  });
+
+  it('responds with json', function(done) {
+    var response = {};
+
+    this.q.promise.done(function() {
+      expect(this.res.json).to.have.been.calledWith(response);
+      done();
+    }.bind(this));
+
+    this.q.resolve(response);
   });
 
 });

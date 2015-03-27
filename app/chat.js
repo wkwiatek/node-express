b5a01d6cc90d372e85694cd25cebcc196af6fa57
wkwiatek <wojtek.kwiatek@gmail.com>
2015-03-27 19:07:29 +0100

    Sending message from client to server.

--- app/chat.js
+++ app/chat.js
@@ -1,5 +1,11 @@
 module.exports = function(io) {
   io.on('connection', function(socket) {
+
     console.log('Hello socket!');
+
+    socket.on('chat message', function(msg) {
+      console.log('msg', msg);
+    });
+
   });
 };
--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,8 @@
 var socket = io();
+
+document.getElementById('newMsgForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  socket.emit('chat message', {
+    msg: document.getElementById('msgInput').value
+  });
+});
